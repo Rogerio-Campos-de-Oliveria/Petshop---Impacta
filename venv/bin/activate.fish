472e39871ae7a3703f5cda4dc3263cb620d02abe
Rogerio-Campos <monge.n@gmail.com>
2026-09-13 13:01:05 -0300

    ajustes

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/rcolinux/Documentos/AC1/Petshop---Impacta/venv
+set -gx VIRTUAL_ENV /home/rcolinux/Documentos/Petshop---Impacta/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
